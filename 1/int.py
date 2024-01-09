7761659e2e85149044c3e92ca8a4c28dcc9aa0f3
Hancooler <76528360+Hancooler@users.noreply.github.com>
2024-01-09 21:42:14 +0700

    Add files via upload

---
+++ 1/int.py
@@ -0,0 +1,15 @@
+# Cetak 7 sebagai sebuah integer
+print (1)
+print (2)
+print (3)
+print (4)
+print (5)
+print (6)
+print (7)
+# Cetak penjumlahan dari 9 dan 3
+print (9+3)
+
+# Cetak '9 + 3' sebagai string
+print ('9+3')
+
+
---
+++ 3/parameter.py
@@ -0,0 +1,10 @@
+# Tambahkan parameter untuk print_hand
+def print_hand(hand):
+    # Gunakan parameter untuk mencetak 'Anda memilih: ___'
+    print('Anda memilih: ' + hand)
+
+# Panggil print_hand dengan 'Batu'
+print_hand('batu')
+
+# Panggil print_hand dengan 'Kertas'
+print_hand('kertas')
